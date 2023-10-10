b50c1114b047faf5433800fa29622f64a70089b5
student <student@192.168.1.25>
2023-10-10 13:58:07 -0700

    progress

--- src/_01_methods/_1_houses/Houses.java
+++ src/_01_methods/_1_houses/Houses.java
@@ -1,4 +1,7 @@
 package _01_methods._1_houses;
+
+import org.jointheleague.graphical.robot.Robot;
+
 /*
  *    Copyright (c) The League of Amazing Programmers 2013-2021
  *    Level 1
@@ -7,5 +10,7 @@ public class Houses {
 		
 	public void run() {
 		// Check the recipe to find out what code to put here
+		Robot r = new Robot();
+		r.goto(x,y);
 	}
 }
