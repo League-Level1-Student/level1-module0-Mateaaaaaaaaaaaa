7abc6f40c0533dbde6f09bef270bb243911d4c59
student <student@192.168.1.25>
2023-10-12 13:58:03 -0700

    PROGRESS

--- src/_01_methods/_1_houses/Houses.java
+++ src/_01_methods/_1_houses/Houses.java
@@ -1,5 +1,8 @@
 package _01_methods._1_houses;
 
+import java.awt.Color;
+import java.util.Random;
+
 import org.jointheleague.graphical.robot.Robot;
 
 /*
@@ -7,10 +10,35 @@ import org.jointheleague.graphical.robot.Robot;
  *    Level 1
  */
 public class Houses {
-		
+	Robot r = new Robot();
+
 	public void run() {
 		// Check the recipe to find out what code to put here
-		Robot r = new Robot();
-		r.goto(x,y);
+
+		r.setSpeed(15);
+		r.moveTo(10, 545);
+		r.penDown();
+		Random ran = new Random();
+		for(int i=0; i<10; i++) {
+			drawHouse(  75+ran.nextInt(200)    );
+			
+		} 
+		
+
+
+	}
+	void drawHouse(int height) {
+
+		r.setPenColor(Color.PINK);
+		r.move(height);
+		r.turn(90);
+		r.move(50);
+		r.turn(90);		
+		r.move(height);
+		r.turn(-90);
+		r.setPenColor(Color.green);
+		r.move(30);
+		r.turn(-90);
+
 	}
 }
