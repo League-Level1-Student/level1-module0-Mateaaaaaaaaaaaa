b3089fa281a57cbdb8aae5fe95c815b9b0d4e2d7
student <student@192.168.1.25>
2023-10-17 14:00:00 -0700

    finish

--- src/_01_methods/_1_houses/Houses.java
+++ src/_01_methods/_1_houses/Houses.java
@@ -14,26 +14,33 @@ public class Houses {
 
 	public void run() {
 		// Check the recipe to find out what code to put here
-
+		r.setWindowColor(Color.black);
 		r.setSpeed(15);
 		r.moveTo(10, 545);
 		r.penDown();
 		Random ran = new Random();
 		for(int i=0; i<10; i++) {
-			drawHouse(  75+ran.nextInt(200)    );
-			
+			//drawHouse(  75+ran.nextInt(200)    );
+			//		drawHouse("small");
+			drawHouse("small", Color.blue);
+			//drawHouse("medium");
+			drawHouse("medium", Color.white);
+			//drawHouse("large");
+			drawHouse("large", Color.red);
 		} 
-		
+
 
 
 	}
 	void drawHouse(int height) {
 
-		r.setPenColor(Color.PINK);
+		//r.setPenColor(Color.);
 		r.move(height);
-		r.turn(90);
-		r.move(50);
-		r.turn(90);		
+		if (height<250) {
+			drawPointyRoof();
+		}else {
+			drawFlatRoof();
+		}
 		r.move(height);
 		r.turn(-90);
 		r.setPenColor(Color.green);
@@ -41,4 +48,64 @@ public class Houses {
 		r.turn(-90);
 
 	}
+	void drawHouse(String height) {
+		if( height.equals("small")) {
+
+			drawHouse (60);
+
+		}
+		if (height.equals("medium")) {
+
+			drawHouse(120);
+
+
+		}
+		if (height.equals("large")) {
+
+			drawHouse(250);
+
+
+		}
+
+	}
+	void drawHouse(String height, Color mycolor) {
+
+
+		r.setPenColor (mycolor);
+
+		if( height.equals("small")) {
+
+			drawHouse (60);
+
+		}
+		if (height.equals("medium")) {
+
+			drawHouse(120);
+
+
+		}
+		if (height.equals("large")) {
+
+			drawHouse(250);
+
+
+		}
+
+	}
+
+	void drawPointyRoof() {
+		r.turn(45);
+		r.move(50);
+		r.turn(90);
+		r.move(50);
+		r.turn(45);
+	}
+
+	void drawFlatRoof() {
+		r.turn(90);
+		r.move(50);
+		r.turn(90);
+
+	}
+
 }
