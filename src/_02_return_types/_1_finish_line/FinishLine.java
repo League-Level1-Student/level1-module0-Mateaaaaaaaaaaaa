7a6f60077bcb2dfff2b538afd7829a049cd76dc2
student <student@192.168.1.25>
2023-11-14 13:58:25 -0800

    progress

--- src/_02_return_types/_1_finish_line/FinishLine.java
+++ src/_02_return_types/_1_finish_line/FinishLine.java
@@ -17,15 +17,21 @@ public class FinishLine {
 	
 	public static void main(String[] args) {
 		//1. Call the drawFinishLine() method
-		
+		 drawFinishLine(); 
+		 
 		//2. Call the crazyMove() method to move the robot
-		
+	crazyMove();
+		 
 		//3. Call the getTotalDistance() and save what is returned into a variable
-		
+	int distance =getTotalDistance(); 
+		 
 		//4. Call the hasCrossedFinishLine() method and save what is returned into a variable
-		
+boolean finishLine =hasCrossedFinishLine();
+		 
 		//5. If the robot has crossed the finish line... 
-			
+			if (finishLine) {
+			JOptionPane.showMessageDialog(null, end);
+			}
 			//6. Use a pop up to say the robot finished and how far it went
 		
 		
