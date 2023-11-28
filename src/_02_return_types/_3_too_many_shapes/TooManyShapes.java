967066c08e77523e1a94f3aed3dced7a089bcc40
student <student@192.168.1.25>
2023-11-28 13:59:57 -0800

    progress

--- src/_02_return_types/_3_too_many_shapes/TooManyShapes.java
+++ src/_02_return_types/_3_too_many_shapes/TooManyShapes.java
@@ -13,38 +13,45 @@ public class TooManyShapes {
 
 	public static void main(String[] args) {
 		//1. Ask the user how many sides they want their shape to be
-	String side=JOptionPane.showInputDialog(null,"How many sides do you want your shade to be?" );
-	int sidesasint = Integer.parseInt(side);
-	
+		String side=JOptionPane.showInputDialog(null,"How many sides do you want your shade to be?" );
+		int sidesasint = Integer.parseInt(side);
+
 		//2. Ask the user how many shapes they want
 		String shape =JOptionPane.showInputDialog(null,"How many shapes do you want?" );
-	int shapeasint = Integer.parseInt(shape);
-			
+		int shapeasint = Integer.parseInt(shape);
+
 		//3. Call canMakeShape() and save what is returned into a variable
 		boolean shapePossible =canMakeShape(sidesasint);
 		//4. If the shape CAN be drawn
-		if()
+		if(shapePossible) {
+
+
 			//5. Call and save what is returned from calculateTurnAngle()
-		
+
+			int angle = calculateTurnAngle(sidesasint);
 			//6. Use drawPolygons() to draw your shape
-		
+			drawPolygons (sidesasint,shapeasint,angle);
+		}
 		//7. If the shape CANNOT be drawn 
-			
+		else {
 			//8. Call notEnoughSides() and print out what is returned 
-		
+			 notEnoughSides();
+		       
+			 
+			 System.out.println(notEnoughSides());        
+		}
 	}
-	
 	static int calculateTurnAngle(int numSides) {
 		int angle = 360/numSides;
 		return angle;
 	}
-	
+
 	static void drawPolygons(int numSides, int numShapes, int degrees) {
 		rob.setSpeed(100);
 		rob.penDown();
 		int dist = 25;
 		Random rand = new Random();
-		
+
 		for(int i = 0; i < numShapes; i++) {
 			int x = rand.nextInt(600);
 			int y = rand.nextInt(600);
@@ -54,22 +61,22 @@ public class TooManyShapes {
 			rob.setY(y);
 			rob.setAngle(angle);
 			rob.setRandomPenColor();
-			
+
 			for(int j = 0; j < numSides; j++) {
 				rob.move(dist);
 				rob.turn(degrees);
 			}
-	    }
+		}
 		rob.hide();
 	}
-	
+
 	static boolean canMakeShape(int numSides) {
 		if(numSides >= 3) {
 			return true;
 		}
 		return false;
 	}
-	
+
 	static String notEnoughSides() {
 		return "I'm sorry we cannot make a shape with that many sides\n"
 				+ "Please enter a number greater than 2";
